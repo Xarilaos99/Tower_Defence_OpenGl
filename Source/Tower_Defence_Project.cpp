0e3c1160c32083989edd3896fe6151ff0d2a3949
Xarilaos99 <ch.xarris26@gmail.com>
2022-12-22 03:51:40 +0200

    Source Init-CMake

---
+++ Source/Tower_Defence_Project.cpp
@@ -0,0 +1,70 @@
+// Include C++ headers
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <glfw3.h>
+
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+// Shader loading utilities and other
+#include <common/shader.h>
+#include <common/util.h>
+#include <common/camera.h>
+#include <common/model.h>
+#include <common/texture.h>
+
+//void initialize();
+//void createContext();
+//void mainLoop();
+//void free();
+//void pollKeyboard(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+#define W_WIDTH 1024
+#define W_HEIGHT 768
+#define TITLE "Lab 05"
+
+// Global variables
+GLFWwindow* window;
+Camera* camera;
+
+#include <fstream>
+
+using namespace std;
+using namespace glm;
+
+// Function prototypes
+
+
+
+int main()
+{
+    while(true)cout << "Ola kala" << endl;
+    //await();
+    //try
+    //{
+    //   /* initialize();
+    //    createContext();
+    //    mainLoop();
+    //    free();*/
+
+    //    cout << "Ola kala" << endl;
+    //}
+    //catch (exception& ex)
+    //{
+    //    cout << ex.what() << endl;
+    //    //getchar();
+    //    /*free();*/
+    //    return -1;
+    //}
+
+    return 0;
+}
